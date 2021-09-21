a4d6801f3660e5a6f7f486d89b7207a0fb25e0d0
Lesly <leslyr562@gmail.com>
2021-09-21 18:57:08 -0500

    adding fatch and url

--- assets/homepage.js
+++ assets/homepage.js
@@ -1,5 +1,11 @@
-var getUsersRepos = function () {
-    fetch("https://api.github.com/users/octocat/repos");
+var getUsersRepos = function (user) {
+    var apiURL = "https://api.github.com/users/" + userfgi + "/repos";
+
+  fetch(apiURL).then(function(response) {
+    response.json().then(function(data){
+        console.log(data);
+    });
+});
 };
 
 getUsersRepos();
